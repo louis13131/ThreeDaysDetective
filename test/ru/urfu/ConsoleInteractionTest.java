de79f4313083bc17baa6e3871ff7313ac6a5b30f
Louis13131 <antipenkovalexandr1993@gmail.com>
2025-10-28 07:40:20 +0300

    Исправлены тесты

--- test/ru/urfu/ConsoleInteractionTest.java
+++ test/ru/urfu/ConsoleInteractionTest.java
@@ -8,96 +8,97 @@ public class ConsoleInteractionTest {
     @Test
     public void processCommandInGameHelpTest() {
         Game game = new Game();
-        ConsoleInteraction consoleInteraction = new ConsoleInteraction();
-        String result = consoleInteraction.processCommandInGame("/help",  game);
+        String result = game.processCommandInGame("/help");
         Assertions.assertEquals(Strings.helpMessage, result);
     }
 
     @Test
     public void getAnswerInGameInfoLidiaTest() {
         Game game = new Game();
-        ConsoleInteraction consoleInteraction = new ConsoleInteraction();
-        String result = consoleInteraction.processCommandInGame("/info Лидия Черткова",  game);
-        Assertions.assertEquals(game.lidiaChertkova.getInfo(), result);
+        String result = game.processCommandInGame("/info Лидия Черткова");
+        String expected = String.format("--- Информация о %s %s --- \n%s лет \nРод занятий: %s \nОписание: %s \nМотив: %s", Strings.lidia);
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
     public void getAnswerInGameInfoAnnaTest() {
         Game game = new Game();
-        ConsoleInteraction consoleInteraction = new ConsoleInteraction();
-        String result = consoleInteraction.processCommandInGame("/info Анна Воронова",  game);
-        Assertions.assertEquals(game.annaVoronova.getInfo(), result);
+        String result = game.processCommandInGame("/info Анна Воронова");
+        String expected = String.format("--- Информация о %s %s --- \n%s лет \nРод занятий: %s \nОписание: %s \nМотив: %s", Strings.anna);
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
     public void getAnswerInGameInfoDmitriyTest() {
         Game game = new Game();
-        ConsoleInteraction consoleInteraction = new ConsoleInteraction();
-        String result = consoleInteraction.processCommandInGame("/info Дмитрий Орлов",  game);
-        Assertions.assertEquals(game.dmitriyOrlov.getInfo(), result);
+        String result = game.processCommandInGame("/info Дмитрий Орлов");
+        String expected = String.format("--- Информация о %s %s --- \n%s лет \nРод занятий: %s \nОписание: %s \nМотив: %s", Strings.dmitriy);
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
     public void getAnswerInGameInfoPetrTest() {
         Game game = new Game();
-        ConsoleInteraction consoleInteraction = new ConsoleInteraction();
-        String result = consoleInteraction.processCommandInGame("/info Пётр Воронов",  game);
-        Assertions.assertEquals(game.petrVoronov.getInfo(), result);
+        String result = game.processCommandInGame("/info Пётр Воронов");
+        String expected = String.format("--- Информация о %s %s --- \n%s лет \nРод занятий: %s \nОписание: %s \nМотив: %s", Strings.petr);
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
     public void getAnswerInGameInfoGrigoriyTest() {
         Game game = new Game();
-        ConsoleInteraction consoleInteraction = new ConsoleInteraction();
-        String result = consoleInteraction.processCommandInGame("/info Григорий Жаров",  game);
-        Assertions.assertEquals(game.grigoriyZharov.getInfo(), result);
+        String result = game.processCommandInGame("/info Григорий Жаров");
+        String expected = String.format("--- Информация о %s %s --- \n%s лет \nРод занятий: %s \nОписание: %s \nМотив: %s", Strings.grigoriy);
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
     public void getAnswerInGameInfoIncorrectNameTest() {
         Game game = new Game();
-        ConsoleInteraction consoleInteraction = new ConsoleInteraction();
-        String result = consoleInteraction.processCommandInGame("/info Иван Иванов", game);
+        String result = game.processCommandInGame("/info Иван Иванов");
         Assertions.assertEquals("Такого персонажа не существует", result);
     }
 
     @Test
     public void getAnswerInGameExitTest() {
         Game game = new Game();
-        ConsoleInteraction consoleInteraction = new ConsoleInteraction();
-        String result = consoleInteraction.processCommandInGame("/exit",  game);
+        String result = game.processCommandInGame("/exit");
         Assertions.assertEquals("Игра завершена", result);
     }
 
     @Test
     public void getAnswerInGameIncorrectCommandTest() {
         Game game = new Game();
-        ConsoleInteraction consoleInteraction = new ConsoleInteraction();
-        String result = consoleInteraction.processCommandInGame("/incorrectCommand",  game);
+        String result = game.processCommandInGame("/incorrectCommand");
         Assertions.assertEquals("Такой команды не существует", result);
     }
 
     @Test
-    public void getAnswerBeforeHelpTest() {
+    public void getAnswerInGameStart_gameTest() {
         Game game = new Game();
-        ConsoleInteraction consoleInteraction = new ConsoleInteraction();
-        String result = consoleInteraction.getAnswerBeforeGame("/help");
+        String result = game.processCommandInGame("/start_game");
+        Assertions.assertEquals("Игра уже началась", result);
+    }
+
+    @Test
+    public void getAnswerBeforeHelpTest() {
+        ProcessCommand processCommand = new ProcessCommand();
+        String result = processCommand.getAnswerBeforeGame("/help");
         Assertions.assertEquals(Strings.helpMessage, result);
     }
 
     @Test
     public void getAnswerBeforeStartTest() {
-        Game game = new Game();
-        ConsoleInteraction consoleInteraction = new ConsoleInteraction();
-        String result = consoleInteraction.getAnswerBeforeGame("/start_game");
+        ProcessCommand processCommand = new ProcessCommand();
+        String result = processCommand.getAnswerBeforeGame("/start_game");
         Assertions.assertEquals("Игра началась", result);
     }
 
     @Test
     public void getAnswerBeforeIncorrectTest() {
-        Game game = new Game();
-        ConsoleInteraction consoleInteraction = new ConsoleInteraction();
-        String result = consoleInteraction.getAnswerBeforeGame("/not_comand");
+        ProcessCommand processCommand = new ProcessCommand();
+        String result = processCommand.getAnswerBeforeGame("/not_comand");
         Assertions.assertEquals("Такой команды не существует", result);
     }
 }
+
