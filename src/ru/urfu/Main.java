5add30cbb27645161b9b64dafbdea3ee3b44f18c
Louis13131 <antipenkovalexandr1993@gmail.com>
2025-11-09 17:16:37 +0300

    Добавлен throws

--- src/ru/urfu/Main.java
+++ src/ru/urfu/Main.java
@@ -1,7 +1,9 @@
 package ru.urfu;
 
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TelegramApiException {
         ProcessMain processMain = new ProcessMain();
         processMain.run();
     }
