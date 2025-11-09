7d61072cc98f2f3da297eeea6df5b61ba983280f
Louis13131 <antipenkovalexandr1993@gmail.com>
2025-11-09 17:16:19 +0300

    Добавлена регистрация бота

--- src/ru/urfu/ProcessMain.java
+++ src/ru/urfu/ProcessMain.java
@@ -1,35 +1,15 @@
 package ru.urfu;
 
 import java.util.Scanner;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class ProcessMain
 {
-    public void run(){
-        ConsoleInteraction helloMessage = new ConsoleInteraction();
-        helloMessage.print(Strings.helloMessage);
-        ConsoleInteraction consoleInteraction = new ConsoleInteraction() ;
-        ProcessCommand processCommand = new ProcessCommand();
-
-        while(true){
-            consoleInteraction.print("Введите команду: ");
-            String instruction = consoleInteraction.getCommand();
-            String answer = processCommand.getAnswerBeforeGame(instruction);
-            consoleInteraction.print(answer);
-            if (answer.equals("Игра началась")){
-                Game game = new Game();
-                consoleInteraction.print(Strings.welcomeMessage);
-                while (true) {
-                    consoleInteraction.print("Введите команду: ");
-                    instruction = consoleInteraction.getCommand();
-                    answer = game.processCommandInGame(instruction);
-                    consoleInteraction.print(answer);
-
-                    if  (answer.equals("Игра завершена")) {
-                        break;
-                    }
-                }
-            }
-        }
+    public void run() throws TelegramApiException {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(new TelegramIntecraction());
     }
 
 }
