7b0c5846747bb36dddbc6a87d4900c7a95cd7862
Louis13131 <antipenkovalexandr1993@gmail.com>
2025-11-11 07:40:13 +0300

    Возращён консольный бот

--- src/main/java/ru/urfu/ProcessMain.java
+++ src/main/java/ru/urfu/ProcessMain.java
@@ -9,6 +9,32 @@ public class ProcessMain
     public void run() throws TelegramApiException {
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
         botsApi.registerBot(new TelegramInteraction());
+
+        ConsoleInteraction helloMessage = new ConsoleInteraction();
+        helloMessage.print(Strings.helloMessage);
+        ConsoleInteraction consoleInteraction = new ConsoleInteraction() ;
+        ProcessCommand processCommand = new ProcessCommand();
+
+        while(true){
+            consoleInteraction.print("Введите команду: ");
+            String instruction = consoleInteraction.getCommand();
+            String answer = processCommand.getAnswerBeforeGame(instruction);
+            consoleInteraction.print(answer);
+            if (answer.equals("Игра началась")){
+                Game game = new Game();
+                consoleInteraction.print(Strings.welcomeMessage);
+                while (true) {
+                    consoleInteraction.print("Введите команду: ");
+                    instruction = consoleInteraction.getCommand();
+                    answer = game.processCommandInGame(instruction);
+                    consoleInteraction.print(answer);
+
+                    if  (answer.equals("Игра завершена")) {
+                        break;
+                    }
+                }
+            }
+        }
     }
 
 }
